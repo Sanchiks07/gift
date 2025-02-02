18b938856500c5fc3bba7d889d10fafba0bda89b
Sanija <ipb23.s.mitniece@vtdt.edu.lv>
2025-02-02 03:13:09 +0200

    jautājumi...

---
+++ quiz/javascript/questions.js
@@ -0,0 +1,627 @@
+// Array of questions grouped by category (30 questions each)
+
+const questions = [
+  {
+    category: "relationship",
+    questions: [
+      {
+        question: "Kad mēs sagājām kopā?",
+        options: ["14.februārī", "20.oktobrī", "7.maijā", "20.septembrī"],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+    ],
+  },
+
+  {
+    category: "future plans",
+    questions: [
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+    ],
+  },
+
+  {
+    category: "about me",
+    questions: [
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+    ],
+  },
+
+  {
+    category: "jokes & secrets",
+    questions: [
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+      {
+        question: "?",
+        options: ["", "", "", ""],
+        correctAnswer: 1,
+      },
+    ],
+  },
+];
